933ce9d3a90a084555604567869d3df4b8b59156
John Brown <bluetac92@gmail.com>
2023-11-22 16:27:37 +0000

    Added -l to put the test in a loop

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/jonnydotgg/tcpcheck
+module github.com/jonnydotgg/tcptest
 
 go 1.21.4
--- main.go
+++ main.go
@@ -26,9 +26,20 @@ func tcp(addr string) (time.Duration, error) {
 }
 
 func main() {
-    if len(os.Args) != 2 {
+    if len(os.Args) < 1 {
         fmt.Println("Expected a target\nExample: jonny.gg:443")
         os.Exit(1)
+    }
+
+    if len(os.Args) == 3 && os.Args[2] == "-l" {
+        for {
+            if t, check := tcp(os.Args[1]); check != nil {
+                fmt.Printf("✘ %s [%s]\n", os.Args[1], t.Round(time.Millisecond))
+            } else {
+                fmt.Printf("✔ %s [%s]\n", os.Args[1], t.Round(time.Millisecond))
+            }
+            time.Sleep(time.Second * 1)
+        }
     }
 
     if t, check := tcp(os.Args[1]); check != nil {
