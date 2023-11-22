07279eb382e1c60d49c91ed2c53050d54ab507e3
John Brown <bluetac92@gmail.com>
2023-11-22 17:15:19 +0000

    allow multiple endpoints in a single command

--- main.go
+++ main.go
@@ -39,14 +39,27 @@ func main() {
         os.Exit(1)
     }
 
-    if len(os.Args) == 3 && os.Args[2] == "-l" {
+    var loop bool
+    var endpoints []string
+    for _, arg := range os.Args[1:] {
+        if arg == "-l" {
+            loop = true
+            continue
+        }
+        endpoints = append(endpoints, arg)
+    }
+
+
+    if loop {
         for {
-            if t, err := tcp(os.Args[1]); err == nil {
-                results(true, os.Args[1], t)
-            } else {
-                results(false, os.Args[1], t)
+            for _, ep := range endpoints {
+                if t, err := tcp(ep); err == nil {
+                    results(true, ep, t)
+                } else {
+                    results(false, ep, t)
+                }
             }
-            time.Sleep(time.Second * 1)
+        time.Sleep(time.Second * 1)
         }
     }
 
